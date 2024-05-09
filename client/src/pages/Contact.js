862ed3a86f151a20b4c54a1d4bb2efd505eece7a
Himanshu Dwivedi <129840650+himanshudwivedi9157@users.noreply.github.com>
2024-05-09 11:23:47 +0530

    ok

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -19,13 +19,13 @@ const Contact = () => {
             vaialible
           </p>
           <p className="mt-3">
-            <BiMailSend /> : www.help@ecommerceapp.com
+            <BiMailSend /> : himanshudwivedi9157@gmail.com.
           </p>
           <p className="mt-3">
-            <BiPhoneCall /> : 012-3456789
+            <BiPhoneCall /> : 6386898043
           </p>
           <p className="mt-3">
-            <BiSupport /> : 1800-0000-0000 (toll free)
+            <BiSupport /> : 6386898043 (toll free)
           </p>
         </div>
       </div>
